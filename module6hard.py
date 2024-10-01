fe683f8c92cc0c6d1dcb79ec0cb9db16cb67f9ff
Ennosto <ennosto_me@mail.ru>
2024-10-01 21:22:33 +0300

    Update module6hard.py

--- module6hard.py
+++ module6hard.py
@@ -1,58 +1,26 @@
 class Figure:
     sides_count = 0
 
-    def __init__(self, __sides: list, __color: list, filled: bool):
-        self.__sides = __sides
-        self.__color = __color
+    def __init__(self, sides: int, color: int, filled: bool):
+        self.__sides = sides
+        self.__color = color
         self.filled = filled
 
     def get_color(self):
         return self.__color
 
-    def __is_valid_color(self, r: int, g: int, b: int):
-        if isinstance(r and g and b, int) and (r and g and b <= 255) and (r and g and b > 0):
-            return True
-
-    def set_color(self, r: int, g: int, b: int):
-        if self.__is_valid_color(r, g, b):
-            self.__color = [r, g, b]
-
-    def __is_valid_sides(self, *args):
-        if isinstance(args, int) and args == self.sides_count and args > 0:
+    def __is_valid_color(self, r, g, b):
+        if isinstance(r, int) and 0 <= r <= 255 and isinstance(g, int) and 0 <= g <= 255 and isinstance(b, int) and 0 <= b <= 255:
             return True
         else:
             return False
 
-    def get_sides(self):
-        return self.__sides
+    def set_color(self, r, g, b):
+        if self.__is_valid_color(r, g, b):
+            self.__color = [r, g, b]
 
-    def __len__(self):
-        return len(self.__sides)
-
-    def set_sides(self, *new_sides):
-        if len(*new_sides) == len(self.sides_count):
-            self.sides_count = self.new_sides
-
-
-class Circle(Figure):
-    sides_count = 0
-    pi = 3.14
-    def __init__(self, __radius):
-        super().__init__(__radius)
-        __radius = self.sides_count // (2 * self.pi)
-
-    def get_square(self):
-        D = self.__radius ** 2
-        S = self.pi * D
-        return S
-
-class Triangle(Figure):
-    sides_count = 3
-    def __init__(self):
-        super().__init__()
-
-    def get_square(self):
-        p = 0.5 * (a + b + c)
-        s_triangle = (p * (p - a) * (p - b) * (p - c)) ** 0.5
-        return s_triangle
-
+    def __is_valid_sides(self, *sides):
+        if isinstance(sides, int) and sides > 0 and len(sides) == self.sides_count:
+            return True
+        else:
+            return False
